4ca77e45d8695a37364cd169e7a69b97cba05141
JerwinRamos <jerwinramos746@gmail.com>
2026-02-01 14:30:59 +0800

    Added the Alarm Feature

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import cv2
 import mediapipe as mp
 import numpy as np
 import pygame
+import time
 from collections import deque
 
 pygame.mixer.init()
@@ -15,6 +16,10 @@ face_mesh = mp.solutions.face_mesh.FaceMesh(refine_landmarks=True) #outputs all 
 HISTORY_LENGTH = 5
 h_ratio_history = deque(maxlen=HISTORY_LENGTH)
 v_ratio_history = deque(maxlen=HISTORY_LENGTH)
+
+start_alarm = 3
+timer = None
+alarm_on = False
 
 while True:
     _, frame = cam.read()
@@ -34,14 +39,12 @@ while True:
         for landmark in landmarks[473:478]: #right eye
             x = int(landmark.x * frame_width)
             y = int(landmark.y * frame_height)
-            cv2.circle(frame, (x, y), 3, (0, 255, 0))
             right_pupil_points.append((x, y))
 
         right_eye_corner_points = []
         for i in (362, 263, 386, 374):
             x = int(landmarks[i].x * frame_width)
             y = int(landmarks[i].y * frame_height)
-            cv2.circle(frame, (x, y), 3, (255, 0, 0))
             right_eye_corner_points.append((x, y))
 
         right_inner = right_eye_corner_points[1]
@@ -54,14 +57,12 @@ while True:
         for landmark in landmarks[468:473]: #left eye
             x = int(landmark.x * frame_width)
             y = int(landmark.y * frame_height)
-            cv2.circle(frame, (x, y), 3, (0, 255, 255))
             left_pupil_points.append((x, y))
 
         left_eye_corner_points = []
         for i in (33, 133, 159, 145):
             x = int(landmarks[i].x * frame_width)
             y = int(landmarks[i].y * frame_height)
-            cv2.circle(frame, (x, y), 3, (255, 0, 0))
             left_eye_corner_points.append((x, y))
 
         left_inner = left_eye_corner_points[1]
@@ -72,6 +73,8 @@ while True:
         # ----------------Calculates center of the Iris----------
         left_pupil_center = np.mean(left_pupil_points, axis=0).astype(int)
         right_pupil_center = np.mean(right_pupil_points, axis=0).astype(int)
+
+
 
         # --------------- HEAD POSITION LANDMARK ----------
         # (LEFT AND RIGHT)
@@ -104,29 +107,30 @@ while True:
         else:
             turn_ratio_y = 0
 
-        head_facing_screen = (turn_ratio_x < 0.80) and (turn_ratio_y < 0.80)
+        head_facing_screen = (1.0 >= turn_ratio_x < 0.80) and (turn_ratio_y < 0.80)
+        print(head_facing_screen, turn_ratio_x, turn_ratio_y)
 
         # Detects what direction your pupils are facing
-        def gaze_ratio_horizontal(iris, left_corner, right_corner):
+        def gaze_ratio_horizontal(left_corner, right_corner, iris):
             eye_width = np.linalg.norm(np.array(right_corner) - np.array(left_corner))
             if eye_width == 0:
                 return 0.5
             return np.linalg.norm(np.array(iris) - np.array(left_corner)) / eye_width
 
-        def gaze_ratio_vertical(iris, top, bottom):
+        def gaze_ratio_vertical(top, bottom, iris):
             eye_height = np.linalg.norm(np.array(bottom) - np.array(top))
             if eye_height == 0:
                 return 0.5
             return np.linalg.norm(np.array(iris) - np.array(top)) / eye_height
 
         # Horizontal iris ratios
-        left_h = gaze_ratio_horizontal(left_pupil_center, left_outer, left_inner)
-        right_h = gaze_ratio_horizontal(right_pupil_center, right_inner, right_outer)
+        left_h = gaze_ratio_horizontal(left_outer, left_inner, left_pupil_center)
+        right_h = gaze_ratio_horizontal(right_inner, right_outer, right_pupil_center)
         avg_h = (left_h + right_h) / 2
 
         # Vertical iris ratios
-        left_v = gaze_ratio_vertical(left_pupil_center, left_upper,left_lower)
-        right_v = gaze_ratio_vertical(right_pupil_center, right_upper, right_lower)
+        left_v = gaze_ratio_vertical(left_upper,left_lower, left_pupil_center,)
+        right_v = gaze_ratio_vertical(right_upper, right_lower, right_pupil_center)
         avg_v = (left_v + right_v) / 2
         # print(avg_v)
 
@@ -135,10 +139,11 @@ while True:
         smooth_h = np.mean(h_ratio_history)
         smooth_v = np.mean(v_ratio_history)
 
-        print(smooth_h, smooth_v)
-
         # Eyes centered thresholds
         eyes_centered = (0.20 < smooth_h < 0.70) and (0.49 < smooth_v < 0.65)
+
+        # cv2.circle(frame, tuple(left_pupil_center), 4, (0, 255, 255), -1)
+        # cv2.circle(frame, tuple(right_pupil_center), 4, (0, 255, 0), -1)
 
         # --- FINAL DECISION ---
         if eyes_centered:
@@ -148,13 +153,28 @@ while True:
         else:
             is_looking_at_screen = False
 
-
         if is_looking_at_screen:
             status_text = "LOOKING AT SCREEN"
         else:
             status_text = "LOOKING AWAY"
-        cv2.putText(frame, status_text, (30, 50),
-                    cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        # cv2.putText(frame, status_text, (30, 50),
+        #             cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+        if not is_looking_at_screen:
+            if timer is None:
+                timer = time.time()
+
+            elapsed = time.time() - timer
+            # cv2.putText(frame, f"AWAY TIMER: {elapsed:.1f}s", (30, 90),
+            #             cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 0), 2)
+            if elapsed >= start_alarm and not alarm_on:
+                pygame.mixer.music.play(-1)
+                alarm_on = True
+        else:
+            timer = None
+            if alarm_on:
+                pygame.mixer.music.stop()
+                alarm_on = False
 
     cv2.imshow(' Focus Buddy ', frame)
     cv2.waitKey(1)
